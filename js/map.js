bd67179ef5ccd89106806c1ea49375d3c77bcbb7
puppyandwen <jiangyiheng0@gmail.com>
2026-04-05 07:45:36 +0800

    1

--- js/map.js
+++ js/map.js
@@ -1,23 +1,87 @@
 const batches = [
   {
-    id: "osaka-2024",
+    id: "osaka-2025",
     lat: 37.6937,
     lon: 135.5023,
     offsetX: 120,
     offsetY: -80,
     scale: 1.4,
-    time: "2024.04.28 - 05.05",
+    time: "2025.04.26 - 05.02",
     photos: [
-      { src: "../images/year2/photo51.jpg", caption: "大阪城" },
-      { src: "../images/year2/photo52.jpg", caption: "道顿堀夜景" },
-      { src: "../images/year2/photo53.jpg", caption: "街头" },
-      { src: "../images/year2/photo53.jpg", caption: "街头" },
-      { src: "../images/year2/photo53.jpg", caption: "街头" },
-      { src: "../images/year2/photo53.jpg", caption: "街头" },
-      { src: "../images/year2/photo53.jpg", caption: "街头" },
-      { src: "../images/year2/photo53.jpg", caption: "街头" }
+      { src: "../images/year3/photo69.jpg", caption: "4月26日 坐上Haruka列车啦~Hello Kitty很可爱呀" },
+      { src: "../images/year3/photo70.jpg", caption: "4月26日 日本的第一口抹茶！就爱这口齁甜！" },
+      { src: "../images/year3/photo71.jpg", caption: "4月26日 史努比松饼！我超级喜欢史努比！" },
+      { src: "../images/year3/photo72.jpg", caption: "4月27日 老京都市区一日游哦~这一天去了清水寺和鸭川！" },
+      { src: "../images/year3/photo73.jpg", caption: "4月28日 坐小火车去看伏见稻荷大社~感觉太阳出来之前阴森森的" },
+      { src: "../images/year3/photo74.jpg", caption: "4月29日 来到大阪，确实是完全不一样的风味呢，很难用国内任何一座其他城市比拟" },
+      { src: "../images/year3/photo75.jpg", caption: "4月29日 我们在大阪住的地方，H2H心斋桥，是在一起以来住过的top理想的环境之一了" },
+      { src: "../images/year3/photo76.jpg", caption: "4月30日 在心斋桥开启新的一天！各种商场都有很多很漂亮的打卡点" },
+      { src: "../images/year3/photo77.jpg", caption: "4月30日 在商场里和史努比的合影~" },
+      { src: "../images/year3/photo78.jpg", caption: "4月30日 标志性游客照1 雯雯雾蓝色的帽子太适合日本这段时间的气候了" },
+      { src: "../images/year3/photo79.jpg", caption: "4月30日 天王寺，紧赶慢赶在日落前找到一个打卡点" },
+      { src: "../images/year3/photo80.jpg", caption: "4月30日 雯雯力竭之前最后陪姜姜逛了一整栋楼的玩具和模型，姜姜感动" },
+      { src: "../images/year3/photo81.jpg", caption: "5月1日 新的一天，依靠蛋酱三明治补充新的能量！在奈良的草地上尽情奔跑释放吧" },
+      { src: "../images/year3/photo82.jpg", caption: "5月2日 乘坐haruka返回大阪关西机场啦，依依不舍中。。。和雯雯的第一次出国，美好的回忆！" }
     ]
-  }
+  },
+  {
+    id: "shanghai 2025",
+    lat: 34,
+    lon: 121,
+    offsetX: 80,
+    offsetY: 40,
+    scale: 1.4,
+    time: "2025.5.3 - 6.30",
+    photos: [
+      { src: "../images/year3/photo83.jpg", caption: "5月9日 从日本回来之后雯雯的常态，太喜欢雯雯无忧无虑睡觉时候的样子了" },
+      { src: "../images/year3/photo84.jpg", caption: "5月20日 和雯雯解锁了新娱乐：台球！" },
+      { src: "../images/year3/photo85.jpg", caption: "5月29日 没有压力的雯雯就是无敌的！就算天天和姜姜嘎嘎吃，依旧身材干柴" },
+      { src: "../images/year3/photo86.jpg", caption: "6月9日 睡不醒的雯，吃不下的蛋糕和不留遗憾的欧国联！在一起之后看得第二精彩的一场球" },
+      { src: "../images/year3/photo87.jpg", caption: "6月28日 夏天到了，和雯雯一起吃了一整个榴莲！" },
+      { src: "../images/year3/photo88.jpg", caption: "6月30日 出发前的一天，雯雯和姜姜还是满心期待又有点隐隐害怕" },
+    ]
+  },
+  {
+    id: "wellington 2025",
+    lat: -43,
+    lon: 175,
+    offsetX: -100,
+    offsetY: -200,
+    scale: 1.4,
+    time: "2025.7.1 - 7.10",
+    photos: [
+      { src: "../images/year3/photo89.jpg", caption: "7月1日 深夜到达惠灵顿，姜对这里的初印象就是这里安安静静的" },
+      { src: "../images/year3/photo90.jpg", caption: "7月2日 休息一天~起床的雯雯开始做咖啡" },
+      { src: "../images/year3/photo91.jpg", caption: "7月2日 第一顿正餐，姜念念不忘的hell pizza" },
+      { src: "../images/year3/photo92.jpg", caption: "7月3日 去上学啦" },
+      { src: "../images/year3/photo93.jpg", caption: "7月3日 晚上参加了开学迎新会，一起蹭饭的一天" },
+      { src: "../images/year3/photo94.jpg", caption: "7月4日 接收雯雯宿舍！当时还只是一个空空荡荡的物资，现在已经满满雯雯气息啦" },
+      { src: "../images/year3/photo95.jpg", caption: "7月5日 新西兰爱心厨房第一弹！煎牛排配贝果！" },
+      { src: "../images/year3/photo96.jpg", caption: "7月6日 姜姜自己出去玩儿，在山顶喝了白咖啡" },
+      { src: "../images/year3/photo97.jpg", caption: "7月8日 爱心厨房第二弹！超级健康的泡面汤配虾虾" },
+      { src: "../images/year3/photo97.jpg", caption: "7月9日 我的愿望是，发现意义或是发现不需要意义，雯雯都要好好地生活！" },
+      { src: "../images/year3/photo98.jpg", caption: "7月9日 姜姜自己出去玩儿，在山顶喝了白咖啡" },
+      { src: "../images/year3/photo99.jpg", caption: "7月9日 雯雯地绒绒帽帽好暖暖，头贴在上面就不想分开了" },
+      { src: "../images/year3/photo100.jpg", caption: "7月10日 乡愁是一片小小的安检区" }
+    ]
+  },
+  {
+    id: "shanghai 2025",
+    lat: 32,
+    lon: 121,
+    offsetX: -100,
+    offsetY: 160,
+    scale: 1.4,
+    time: "2025.11.21 - 12.7",
+    photos: [
+      { src: "../images/year3/photo101.jpg", caption: "11月21日 雯雯如闪电般归来！" },
+      { src: "../images/year3/photo102.jpg", caption: "11月24日 休息了一个周末，雯雯开始回归图书馆" },
+      { src: "../images/year3/photo103.jpg", caption: "11月26日 下班之后，一起去看疯狂动物城首映吧" },
+      { src: "../images/year3/photo104.jpg", caption: "11月30日 吼吼，啥好事都给雯雯碰着了！一起看到了齐达内大活人！" },
+      { src: "../images/year3/photo105.jpg", caption: "12月5日 带雯雯爸爸妈妈一起逛了交大" },
+      { src: "../images/year3/photo106.jpg", caption: "12月7日 要怎么收藏，要怎么拥有？" },
+    ]
+  },
 ];
 
 
